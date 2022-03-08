6700f268ece20b6d0db2fad64e1b7bc7796fffa8
Clem <59027185+clementereceno@users.noreply.github.com>
2022-03-08 10:09:29 -0500

    add weight unit received
    add weight unit received

--- NGCM/debug.h
+++ NGCM/debug.h
@@ -5,8 +5,48 @@
  *  Author: clemente
  */ 
 
-#ifndef UART_BUF_SIZE
-#define UART_BUF_SIZE            ((uint8_t)50)
-#endif
+#ifndef DEBUG_H_
+#define DEBUG_H_
 
-char Uart_DtxBuf[UART_BUF_SIZE];
+#include "System.h"
+
+
+
+typedef struct {
+	union {
+		struct {
+			union {
+				struct {
+					int8_t			weightReceived;
+					uint8_t			ntused01;
+					uint8_t			ntused02;
+					uint8_t			ntused03;
+					uint8_t			ntused04;
+					uint8_t			ntused05;
+					
+					union {
+						struct {
+							uint8_t		ntusedbit0		:1;	//b0
+							uint8_t		ntusedbit1		:1;
+							uint8_t		ntusedbit2		:1;
+							uint8_t		ntusedbit3		:1;
+							uint8_t		ntusedbit4		:1;
+							uint8_t		ntusedbit5		:1;
+							uint8_t		ntusedbit6		:1;
+							uint8_t		ntusedbit7		:1;	//b7
+						};
+						uint8_t	ntused06;
+					};
+				};
+				uint8_t		packet3[7];
+			};
+		};
+		uint8_t dbytes[7];
+	};
+} DB_TX_PACKET;
+
+
+void Debug_serializeTxPacket(char *dest);
+
+
+#endif
